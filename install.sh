7374d61ee5b92e21303b1a3cb56da74c5022fa20
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-12-10 14:58:20 -0200

    fix realpath resolution

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SW_URL=https://raw.githubusercontent.com/gabrielchiconi/sw/master/sw.py
-SW_PATH=$(realpath ~/.sw/bin)
+SW_PATH=$(realpath ~)/.sw/bin
 
 mkdir -p $SW_PATH
 echo "Downloading sw..."
