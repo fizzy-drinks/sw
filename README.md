a7a0440c42a510379c392f453603739d117b8212
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2018-12-10 14:46:14 -0200

    update to installation / add README

---
+++ README.md
@@ -0,0 +1,11 @@
+# sw
+
+> Super simple SSH keyring for your servers!
+
+## Usage
+
+    curl -sSL https://raw.githubusercontent.com/gabrielchiconi/sw/master/install.sh | bash
+
+
+
+
--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@ SW_URL=https://raw.githubusercontent.com/gabrielchiconi/sw/master/sw.py
 SW_PATH=$(realpath ~/.sw/bin)
 
 mkdir -p $SW_PATH
+echo "Downloading sw..."
 curl -o $SW_PATH/sw $SW_URL
+echo "Done. Adding to rc..."
 echo "
-PATH=$PATH:$SW_PATH" >> ~/.bashrc
+PATH=\"$SW_PATH:\$PATH\"" >> ~/.bashrc
+echo "Done"
+echo "Restart your shell to use sw!"
+
